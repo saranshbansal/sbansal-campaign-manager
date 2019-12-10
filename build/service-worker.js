03dd29178e370635f7759ed4beb541ab663e77ee
Saransh Bansal <saran3h.bansal@gmail.com>
2019-12-10 17:25:52 +0530

    remove build artifacts and test config changes.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b15261784f30f4854df12d2013a2085.js"
+  "/precache-manifest.c0382b432e31d6a0592f9847bf1e29f6.js"
 );
 
 self.addEventListener('message', (event) => {
--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ app.use(bodyParser.json());
 require('./routes/CampaignRoutes')(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
