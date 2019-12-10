0dba491e838b126444f1c26488a07ee6932be67f
Saransh Bansal <saran3h.bansal@gmail.com>
2019-12-10 18:10:54 +0530

    test server changes.

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ app.use(bodyParser.json());
 require('./routes/CampaignRoutes')(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static(__dirname));
 
   const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "index.html"));
   });
 }
 
