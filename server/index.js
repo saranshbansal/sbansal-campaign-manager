1ac4038a2ed9334120bd2bc7536addb65f007a99
Saransh Bansal <saran3h.bansal@gmail.com>
2019-12-11 20:03:39 +0530

    test server fix.

--- server/index.js
+++ server/index.js
@@ -39,9 +39,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 
   const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  // });
 }
 
 const PORT = process.env.PORT || 5000;
